b2fe7e1ba5928223fa0a1d670c9d8984fa6e7a3d
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-05 23:31:59 +0300

    caption animations added

--- src/components/main/header/Captions.tsx
+++ src/components/main/header/Captions.tsx
@@ -6,16 +6,22 @@ import classes from "./Captions.module.css";
 const captions = [
   { id: 0, title: "For human rights have resulted." },
   { id: 1, title: "I never dreamed about success. I worked for it." },
-  { id: 2, title: "Don’t let yesterday take up too much of today." },
+  { id: 2, title: "." },
 ];
 
 export default function Captions() {
-  const [id, setId] = useState(0);
-
   return (
     <div className={classes.container}>
-      <p>Whereas disregard and contempt</p>
-      <p>For human rights have resulted</p>
+      <p className={classes.doubleUp} style={{ top: "2.5rem" }}>
+        For human rights have resulted.
+      </p>
+      <p className={classes.up} style={{ top: "2.5rem" }}>
+        Don’t let yesterday take up too much of today
+      </p>
+      <span className={`${classes.bracket} ${classes.grow}`}></span>
+      <p className={classes.down} style={{ top: "4.7rem" }}>
+        For human rights have resulted.
+      </p>
     </div>
   );
 }
