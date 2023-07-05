a09581fd608a0f0d894262d76189447c044e8db1
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-05 15:04:53 +0300

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,11 +2,19 @@ import Link from "next/link";
 import classes from "./Footer.module.css";
 import InfoList from "./InfoList";
 import SocialContainer from "./SocialContainer";
+import Image from "next/image";
+import logo from "../../../public/logo.png";
 
 export default function Footer() {
   return (
     <footer className={classes.footer}>
-      <SocialContainer />
+      <div className={classes.container}>
+        <Link href={"/"}>
+          <Image src={logo} width={200} height={70} alt="logo" quality={50} />
+        </Link>
+        <SocialContainer />
+      </div>
+
       <InfoList header="İletişim Bilgileri">
         <li>
           <Link href={"/"}>Adres</Link>
