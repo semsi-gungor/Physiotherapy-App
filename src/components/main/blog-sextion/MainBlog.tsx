132ea717b38682ad128bcff96ca2e464f15bbb5b
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-06 19:44:53 +0300

    added blog animations

--- src/components/main/blog-sextion/MainBlog.tsx
+++ src/components/main/blog-sextion/MainBlog.tsx
@@ -4,16 +4,26 @@ import classes from "./MainBlog.module.css";
 import Image from "next/image";
 import blog from "../../../../public/bg-1.jpg";
 import FeaturedPost from "./FeaturedPost";
+import { useInView } from "react-intersection-observer";
 
 export default function MainBlog() {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
   return (
-    <div className={classes.blog}>
-      <div className={classes.posts}>
+    <div className={classes.blog} ref={ref}>
+      <div className={`${classes.posts} ${inView ? classes.slideInLeft : ""}`}>
         <FeaturedPost />
         <FeaturedPost />
         <FeaturedPost />
       </div>
-      <div className={classes.imageContainer}>
+      <div
+        className={`${classes.imageContainer} ${
+          inView ? classes.slideInRight : ""
+        }`}
+      >
         <Image src={blog} alt="blog" />
       </div>
     </div>
