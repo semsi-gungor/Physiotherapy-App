e722e34755858b3818e3c6a60896d18675cbcc2e
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-12 15:38:23 +0300

    quote input for blog

--- src/app/blog-ekle/page.tsx
+++ src/app/blog-ekle/page.tsx
@@ -3,12 +3,13 @@ import Modal from "@/components/create-post/Modal/Modal";
 import HeaderInput from "@/components/create-post/HeaderInput/HeaderInput";
 import TextInput from "@/components/create-post/TextInput/TextInput";
 import ListInput from "@/components/create-post/ListInput/OrderedListInput";
+import QuoteInput from "@/components/create-post/QuoteInput/QuoteInput";
 
 const Page: FC = () => {
   return (
     <div>
       <Modal>
-        <ListInput />
+        <QuoteInput />
       </Modal>
     </div>
   );
