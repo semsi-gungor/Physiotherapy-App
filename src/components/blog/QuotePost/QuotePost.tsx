82d106e15c7bc06e60e2e6d2f23a3b979115e6c2
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-13 00:46:23 +0300

    quote post created

--- src/components/blog/QuotePost/QuotePost.tsx
+++ src/components/blog/QuotePost/QuotePost.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import classes from "./QuotePost.module.css";
+import classes from "./TextPost.module.css";
 import { BlogPart } from "@/types/blog-posts";
 
 interface QuotePostProps {
@@ -7,7 +7,23 @@ interface QuotePostProps {
 }
 
 const QuotePost: FC<QuotePostProps> = ({ post }) => {
-  return <div className={classes.container}></div>;
+  const content = post.postContent;
+  const color = post.options?.color;
+  const align = post.options?.textAlignment;
+
+  let textAlign = classes.left;
+
+  if (align === "center") {
+    textAlign = classes.center;
+  } else if (align === "right") {
+    textAlign = classes.right;
+  }
+
+  return (
+    <p className={`${classes.text} ${textAlign}`} style={{ color: `${color}` }}>
+      {content}
+    </p>
+  );
 };
 
 export default QuotePost;
