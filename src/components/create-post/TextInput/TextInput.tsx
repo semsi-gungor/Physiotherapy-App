20b5548bd2d4e9567d80187f2fce82a6bd5c772e
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-12 13:58:35 +0300

    text input component for blog

--- src/components/create-post/TextInput/TextInput.tsx
+++ src/components/create-post/TextInput/TextInput.tsx
@@ -4,10 +4,15 @@ import { FC } from "react";
 import classes from "./TextInput.module.css";
 import TextareaInput from "@/components/ui/input/TextareaInput";
 import { useForm, FieldValues } from "react-hook-form";
+import ToolBar from "../ToolBar/ToolBar";
+import ToolBarDivider from "../ToolBar/ToolBarDivider";
+import ToolBarSection from "../ToolBar/ToolBarSection";
+import ColorInput from "@/components/ui/input/ColorInput";
+import RadioInput from "@/components/ui/input/RadioInput";
+import { BsTextLeft, BsTextCenter, BsTextRight } from "react-icons/bs";
+import Button from "@/components/ui/button/Button";
 
-interface TextInputProps {}
-
-const TextInput: FC<TextInputProps> = ({}) => {
+const TextInput: FC = ({}) => {
   const form = useForm({ mode: "all" });
   const { register, handleSubmit, formState } = form;
 
@@ -17,14 +22,45 @@ const TextInput: FC<TextInputProps> = ({}) => {
     console.log("submitted", data);
   }
   return (
-    <div className={classes.container}>
+    <form className={classes.container} onSubmit={handleSubmit(onSubmit)}>
+      <ToolBar>
+        <ToolBarSection>
+          <ColorInput name="color" register={register} />
+        </ToolBarSection>
+        <ToolBarDivider />
+        <ToolBarSection>
+          <RadioInput
+            id="left"
+            name="textAlignment"
+            register={register}
+            value="left"
+            icon={BsTextLeft}
+          />
+          <RadioInput
+            id="center"
+            name="textAlignment"
+            register={register}
+            value="center"
+            icon={BsTextCenter}
+          />
+          <RadioInput
+            id="right"
+            name="textAlignment"
+            register={register}
+            value="right"
+            icon={BsTextRight}
+          />
+        </ToolBarSection>
+      </ToolBar>
       <TextareaInput
         register={register}
         label="Eklenecek metni giriniz"
         name="text"
         errorMessage={errors.text?.message?.toString()}
       />
-    </div>
+
+      <Button size="md">Gönder</Button>
+    </form>
   );
 };
 
--- src/components/ui/input/RadioInput.tsx
+++ src/components/ui/input/RadioInput.tsx
@@ -1,15 +1,25 @@
 import { FC } from "react";
 import classes from "./RadioInput.module.css";
 import { UseFormRegister, FieldValues } from "react-hook-form";
+import { IconType } from "react-icons/lib";
 
 interface RadioInputProps {
   name: string;
   register: UseFormRegister<FieldValues>;
   value: string;
-  label: string;
+  label?: string;
+  icon?: IconType;
+  id: string;
 }
 
-const RadioInput: FC<RadioInputProps> = ({ name, register, value, label }) => {
+const RadioInput: FC<RadioInputProps> = ({
+  name,
+  register,
+  value,
+  label,
+  icon: Icon,
+  id,
+}) => {
   const inputRegister = register(name, {
     required: { value: true, message: "" },
   });
@@ -18,12 +28,13 @@ const RadioInput: FC<RadioInputProps> = ({ name, register, value, label }) => {
       <input
         value={value}
         className={classes.input}
-        id={label}
+        id={id}
         type="radio"
         {...inputRegister}
       ></input>
-      <label htmlFor={label} className={classes.label}>
-        {label}
+      <label htmlFor={id} className={classes.label}>
+        {label && label}
+        {Icon && <Icon />}
       </label>
     </div>
   );
