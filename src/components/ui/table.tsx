9bd5d324b6816e12f9c361a77a44409fbdec336e
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-18 14:39:09 +0300

    data table component

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -4,9 +4,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="w-full overflow-auto">
-    <table ref={ref} className={className} {...props} />
-  </div>
+  <table ref={ref} className={className} {...props} />
 ));
 Table.displayName = "Table";
 
