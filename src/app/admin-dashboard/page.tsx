ca4ec3696ef9eff4077439c1385d40f5694be266
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-19 22:51:25 +0300

    old section carried

--- src/app/admin-dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import Wrapper from "@/components/ui/single-page-wrapper/Wrapper";
-import MainPage from "@/components/admin-page/MainPage/MainPage";
-
-const Page: FC = () => {
-  return <MainPage />;
-};
-
-export default Page;
