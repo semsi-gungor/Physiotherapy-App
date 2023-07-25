1573d52984aa755ce6bb5258e5fb8f4e10c984fd
Semsi Gungor <semsi.gungor00@gmail.com>
2023-07-25 23:35:22 +0300

    updates

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { MessageSchema } from "@/models/Message";
-import { z } from "zod";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -8,22 +7,13 @@ export async function POST(req: NextRequest) {
   const parsedMessage = MessageSchema.safeParse(body);
 
   if (parsedMessage.success) {
-    return NextResponse.json({ message: "Message posted succsessfully." });
+    return NextResponse.json(
+      { message: "Message posted succsessfully." },
+      { status: 200 }
+    );
   } else {
     const errors = parsedMessage.error.formErrors.fieldErrors;
-    const newErrorResponse = new Response(
-      JSON.stringify({
-        error: errors,
-      }),
-      {
-        status: 400,
-        statusText: "Bad Request",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
 
-    return newErrorResponse;
+    return NextResponse.json({ errorMessage: errors }, { status: 400 });
   }
 }
