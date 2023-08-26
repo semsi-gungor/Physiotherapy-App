8fdef569da8784154b3cb67259f69e58462127c6
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 15:01:09 +0300

    main page route

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,27 +1,37 @@
-import classes from "./page.module.css";
+import { FC } from "react";
 import Header from "@/components/main/header/Header";
 import MainWrapper from "@/components/ui/main-wrapper/MainWrapper";
 import ServiceCardContainer from "@/components/main/services-section/ServiceCardContainer";
-import MainBlog from "@/components/main/blog-section/MainBlog";
-import Title from "@/components/main/title/Title";
-import CommentsContainer from "@/components/main/comments/CommentsContainer";
-import { LuHelpingHand, LuPencil } from "react-icons/lu";
-import { FaRegComment } from "react-icons/fa";
+import ServiceHeader from "@/components/main/services-section/ServiceHeader";
+import Entrance from "@/components/main/entrance/Entrance";
+import Familiar from "@/components/main/familiar/Familiar";
+import Promises from "@/components/main/promises/Promises";
+import BlogPosts from "@/components/main/blog/BlogPosts";
+import Comments from "@/components/main/new-comments/Comments";
 
-export default function Home() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ana Sayfa",
+  description:
+    "LAVİNİS Egzersiz ve Danışmanlık Merkezi olarak danışanlarımızın yaşam kalitesini artırmak ve onlara ağrısız bir yaşam sunmak için profesyonel bir çatı altında 2020 yılında hizmet hayatına başladık.LAVİNİS bünyesinde; Reformer Pilates, Hamile Pilatesi, Yogaterapi, Manuel Terapi, Pelvik Taban Rehabilitasyonu, 3 Boyutlu Skolyoz Egzersizleri, Lenfödem Rehabilitasyonu, Kanser Sonrası Egzersiz Danışmanlığı, Dans ve Zumba hizmetlerini vermekteyiz.",
+};
+
+const Home: FC = ({}) => {
   return (
-    <main className={classes.main}>
+    <main>
       <Header />
-      <Title title="hizmetler" icon={LuHelpingHand} />
+      <Entrance />
+      <Familiar />
+      <Promises />
+      <ServiceHeader />
       <MainWrapper>
         <ServiceCardContainer />
       </MainWrapper>
-      <Title title="blog" icon={LuPencil} />
-      <MainBlog />
-      <Title title="yorumlar" icon={FaRegComment} />
-      <MainWrapper>
-        <CommentsContainer />
-      </MainWrapper>
+      <BlogPosts />
+      <Comments />
     </main>
   );
-}
+};
+
+export default Home;
