bf8854c378e01999aafea84b7e65ba7f0a710ed7
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 14:54:52 +0300

    personne blog route

--- src/app/(doctor)/personnel/blog/page.tsx
+++ src/app/(doctor)/personnel/blog/page.tsx
@@ -1,9 +1,22 @@
 import { FC } from "react";
+import WriterPage from "@/components/create-post/WriterPage/WriterPage";
+import UIContextProvier from "@/context/uiControl";
+import BlogContextProvider from "@/context/blogContext";
+import QueryProvider from "@/components/providers/QueryProvider/QueryProvider";
+import BlogPage from "@/components/personnel-page/Blog/BlogPage";
 
 interface Props {}
 
 const Page: FC<Props> = ({}) => {
-  return <div></div>;
+  return (
+    <BlogContextProvider>
+      <UIContextProvier>
+        <QueryProvider>
+          <BlogPage />
+        </QueryProvider>
+      </UIContextProvier>
+    </BlogContextProvider>
+  );
 };
 
 export default Page;
