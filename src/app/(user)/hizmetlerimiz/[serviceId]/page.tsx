1ccc9a6ef5d9ef7bfc60afa7494949f35c03d3df
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 14:59:45 +0300

    services route

--- src/app/(user)/hizmetlerimiz/[serviceId]/page.tsx
+++ src/app/(user)/hizmetlerimiz/[serviceId]/page.tsx
@@ -1,12 +1,77 @@
+export const revalidate = 60;
+
 import { FC } from "react";
 import ServicePage from "@/components/services/ServicePage/ServicePage";
+import QueryProvider from "@/components/providers/QueryProvider/QueryProvider";
+import { Metadata, ResolvingMetadata } from "next";
+import client from "@/lib/prisma";
+import SessionProvider from "@/components/providers/SessionProvider/SessionProvider";
 
 type PageProps = {
   params: { serviceId: string };
 };
 
-const Page: FC<PageProps> = ({ params }) => {
-  return <ServicePage serviceId={params.serviceId} />;
+export async function generateMetadata(
+  { params }: PageProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.serviceId;
+
+  // fetch data
+  const service = await client.service.findUnique({
+    where: {
+      id: id,
+    },
+
+    select: {
+      body: true,
+      title: true,
+    },
+  });
+
+  return {
+    title: service?.title,
+    description: service?.body,
+  };
+}
+
+export async function generateStaticParams() {
+  const ids = await client.service.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  return ids.map((id) => {
+    return {
+      serviceId: id.id,
+    };
+  });
+}
+
+const Page: FC<PageProps> = async ({ params }) => {
+  const service = await client.service.findUnique({
+    where: {
+      id: params.serviceId,
+    },
+    select: {
+      body: true,
+      bodyImage: true,
+      definition: true,
+      headerImage: true,
+      title: true,
+      treatments: true,
+    },
+  });
+
+  return (
+    <SessionProvider>
+      <QueryProvider>
+        {service && <ServicePage service={service} />}
+      </QueryProvider>
+    </SessionProvider>
+  );
 };
 
 export default Page;
--- src/app/(user)/hizmetlerimiz/page.tsx
+++ src/app/(user)/hizmetlerimiz/page.tsx
@@ -1,10 +1,36 @@
 import { FC } from "react";
 import ServicesPage from "@/components/services/ServicesPage";
+import ServicesGrid from "@/components/services/NewServices/ServicesGrid";
+import client from "@/lib/prisma";
+import { Metadata } from "next";
+
+export const revalidate = 60;
+
+export const metadata: Metadata = {
+  title: "Hizmetlerimiz - Pyhsical Therapy",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio officiis ratione, accusamus corrupti voluptate cum ducimus.",
+};
 
 interface Props {}
 
-const Page: FC<Props> = ({}) => {
-  return <ServicesPage />;
+const Page: FC<Props> = async ({}) => {
+  const services = await client.service.findMany();
+
+  const servicesGrid = services.map((service) => {
+    return {
+      id: service.id,
+      title: service.title,
+      src: service.bodyImage,
+    };
+  });
+
+  return (
+    <div>
+      <ServicesPage />
+      <ServicesGrid services={servicesGrid} />
+    </div>
+  );
 };
 
 export default Page;
