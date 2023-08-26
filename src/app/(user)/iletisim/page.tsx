fdc8903a7aaedcd987ac6ce015f8f33aef26be1c
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 15:00:03 +0300

    cantact route

--- src/app/(user)/iletisim/page.tsx
+++ src/app/(user)/iletisim/page.tsx
@@ -1,5 +1,25 @@
+import { FC } from "react";
 import ContactPage from "@/components/contact/ContactPage";
+import { Poppins } from "next/font/google";
+import { Metadata } from "next";
 
-export default function Contact() {
-  return <ContactPage />;
-}
+export const metadata: Metadata = {
+  title: "İletişim - Pyhsical Therapy",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio officiis ratione, accusamus corrupti voluptate cum ducimus.",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+const Contact: FC = () => {
+  return (
+    <div className={poppins.className}>
+      <ContactPage />
+    </div>
+  );
+};
+
+export default Contact;
