7de0dc48a9981020a96b1c65bfe87d927e59c7f8
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 15:00:22 +0300

    appointment route

--- src/app/(user)/randevu-alma/page.tsx
+++ src/app/(user)/randevu-alma/page.tsx
@@ -1,8 +1,20 @@
 import { FC } from "react";
 import AppointmentPage from "@/components/appointment/AppointmentPage";
+import QueryProvider from "@/components/providers/QueryProvider/QueryProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Randevu Al - Pyhsical Therapy",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio officiis ratione, accusamus corrupti voluptate cum ducimus.",
+};
 
 const Page: FC = ({}) => {
-  return <AppointmentPage />;
+  return (
+    <QueryProvider>
+      <AppointmentPage />
+    </QueryProvider>
+  );
 };
 
 export default Page;
