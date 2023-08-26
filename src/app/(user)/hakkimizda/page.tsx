b37a0d26b982998f9fd7fd8b4eac8d413193b60f
Semsi Gungor <semsi.gungor00@gmail.com>
2023-08-26 14:59:36 +0300

    about route

--- src/app/(user)/hakkimizda/page.tsx
+++ src/app/(user)/hakkimizda/page.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react";
 import AboutPage from "@/components/about/AboutPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hakkımızda - Pyhsical Therapy",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore similique fugit laborum quas impedit, adipisci quod velit eveniet alias atque dolorem soluta non voluptatem ipsum. Sint ipsam delectus ex eligendi?",
+};
 
 const Page: FC = ({}) => {
   return <AboutPage />;
